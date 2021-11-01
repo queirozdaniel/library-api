cc3d5a72899a678bb482d38241f310cee01b9a18
queirozdaniel <danielqueirozak77@gmail.com>
2021-11-01 01:12:15 -0300

    feat: Mapeado classe com JPA

--- src/main/kotlin/com/danielqueiroz/libraryapi/domain/model/Book.kt
+++ src/main/kotlin/com/danielqueiroz/libraryapi/domain/model/Book.kt
@@ -1,7 +1,15 @@
 package com.danielqueiroz.libraryapi.domain.model
 
+import javax.persistence.*
+
+@Entity
+@Table
 data class Book(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
     val title: String,
     val author: String,
     val isbn: String
